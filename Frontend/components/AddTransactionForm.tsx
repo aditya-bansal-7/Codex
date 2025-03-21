8aec4fb33db07867c6689920f1c08fcbdfe513c7
Aditya Bansal <adityabansalsafe@gmail.com>
2025-03-21 21:40:43 +0530

    add : backend url

--- Frontend/components/AddTransactionForm.tsx
+++ Frontend/components/AddTransactionForm.tsx
@@ -189,7 +189,7 @@ export default function TransactionPage() {
 
       console.log(data)
       // Simulate API call
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch(`${process.env.BACKEND_URL}/predict`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
